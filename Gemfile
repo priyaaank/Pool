35bfe119ac79fb64ac3e14bb2125750f593881ed
Priyank Gupta <priyaaank@gmail.com>
2011-12-27 21:31:47 +0530

    Adding first cut of the app

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.10'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'bson_ext', "1.5.2"
+gem 'jquery-rails'
+
+group :development do
+  gem 'rspec'
+end
+
+gem 'mongoid'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pool::Application.config.secret_token = 'f81c74395d06a7c203d9bf3df8dee2b0ec3370c0474516858194a5b36724aac9c7ad2aebc37a9c09360a1b57519ba8d47ed52b24e09d0e05657af1fc9fef1f1c'
